b8428fe15450e6366f97bd653ab5780bb9768df8
linnminhtet23 <linnminhtet23@gmail.com>
2021-09-19 20:49:44 +0630

    9/19/21

---
+++ feedback/js/app.js
@@ -0,0 +1,10 @@
+const panel = document.getElementById('panel');
+const ratingcontainer = document.querySelector('.rating-container');
+const ratings = document.querySelectorAll('.rating');
+const sendbtn = document.querySelector('#send');
+
+
+panel.addEventListener('click',(e)=>{
+    // console.log('click')
+    // console.log(e.target)
+})
