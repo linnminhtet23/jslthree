0c3353c13d7866fb17b5bc30f5062146f9e25716
linnminhtet23 <linnminhtet23@gmail.com>
2021-09-25 17:35:15 +0630

    feedback done

--- feedback/js/app.js
+++ feedback/js/app.js
@@ -1,10 +1,46 @@
 const panel = document.getElementById('panel');
-const ratingcontainer = document.querySelector('.rating-container');
+const ratingcontainer = document.querySelector('.ratings-container');
 const ratings = document.querySelectorAll('.rating');
 const sendbtn = document.querySelector('#send');
 
+let selectedrating = 'Satisfied'
 
-panel.addEventListener('click',(e)=>{
+ratingcontainer.addEventListener('click',(e)=>{
+
+    // console.log('hay') 
+    // if(e.target.classList.contains('rating')){{}
+    //     console.log(e.target);
+    // }
+
+    if(e.target.parentNode.classList.contains('rating')){
+        // console.log(e.target);
+
+        removeactive();
+        e.target.parentNode.classList.add('active');
+
+        // selectedrating = e.target.nextElementSibling.textContent;
+        selectedrating = e.target.parentNode.lastElementChild.textContent;
+        // console.log(selectedrating);
+    }else if(e.target.classList.contains('rating')){
+        removeactive();
+        e.target.classList.add('active');
+        selectedrating  =e.target.lastElementChild.textContent;
+    }
+});
+
+function removeactive(){
+    for(let i=0; i< ratings.length; i++){
+        ratings[i].classList.remove('active');
+    }
+}
+
+sendbtn.addEventListener('click',()=>{
     // console.log('click')
     // console.log(e.target)
+    panel.innerHTML=`
+    <i class="fas fa-heart"> </i>
+    <strong>Thank You</strong>
+    <strong>Feed back: ${selectedrating}</strong>
+    <p>We'll use your feeback to improve our customer support</p>
+    `;
 })
