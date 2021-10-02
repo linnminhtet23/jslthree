cd3cd4d6db149500bd5b5f72e985f2b62f67ffff
linnminhtet23 <linnminhtet23@gmail.com>
2021-10-02 13:26:23 +0630

    theme clock

--- draganddrop/js/app.js
+++ draganddrop/js/app.js
@@ -13,7 +13,56 @@ function dragstart(){
 
     // this.classList.add('hold')
     this.className += ' hold';
+    setTimeout(()=>{
+        this.className='invisible';
+    },0);
 }
+
+empties.forEach(empty=>{
+    // console.log(empty);
+
+    empty.addEventListener('dragover', dragover);
+    empty.addEventListener('dragenter', dragenter);
+    empty.addEventListener('dragleave',dragleave);
+    empty.addEventListener('drop', dragdrop);
+});
+
+function dragover(e){
+    console.log('drag over');
+    e.preventDefault();
+}
+function dragenter(e){
+    console.log('drag enter');
+    e.preventDefault();
+
+    this.className +=' hovered';
+};
+
+function dragleave(e){
+    console.log('drag leave');
+    e.preventDefault();
+
+    this.className='empty'
+}
+
+function dragdrop(e){
+    console.log('drag drop');
+    // e.preventDefault();
+
+    this.className='empty';
+    this.appendChild(picdiv);
+
+}
+
+// Es6 can looping object5-specific iterator value
+// for(const empty of empties){
+//     console.log(empty);
+// }
+
+// =>numerable property name of an object 
+// for(const empty in empties){
+//     console.log(empty);
+// }
 
 //DRAG END
 function dragend(){
